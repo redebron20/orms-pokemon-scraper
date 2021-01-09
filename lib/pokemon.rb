233d0fc3a417c45a417a988e6e14442ff793f853
redebron20 <redebron20@gmail.com>
2021-01-09 17:59:34 -0500

    commit tree 307467247f527da1c10f50a1807fb9d674517fa8

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = "INSERT INTO pokemon(name, type) VALUES (?, ?);"
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id_num, db)
+        sql = "SELECT * FROM pokemon WHERE pokemon.id = ?;"
+        info = db.execute(sql, id_num)[0]
+        Pokemon.new(id: info[0], name: info[1], type: info[2], db: db)
+    end
+
+
+
 end
